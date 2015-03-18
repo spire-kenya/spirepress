4dae1ce5950a82b1075c76ecab83c6bc6879f766
Monji Dolon <mdolon@gmail.com>
2015-03-18 10:40:30 -0400

    Add JS animation for heading

--- js/spire.js
+++ js/spire.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
-  
+
+  $('.navbar-header').data('size','big');
+
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 0) {
+      if($('.navbar-header').data('size') == 'big') {
+        $('.navbar-header').data('size','small').css("height", 60);
+        $('.navbar-brand').css("maxWidth", 100);
+        $('.navbar-right').css("top", 7);
+        $('.splash').animate({ 'marginTop': '60'}, 300);
+        // $('.navbar-header').stop().animate({
+        //     height:'60px'
+        // }, 600);
+      }
+    } else {
+      if($('.navbar-header').data('size') == 'small') {
+        $('.navbar-header').data('size','big').css("height", 100);
+        $('.navbar-brand').css("maxWidth", 180);
+        $('.navbar-right').css("top", 25);
+        $('.splash').delay(250).animate({ 'marginTop': '100'}, 300);
+        // $('.navbar-header').stop().animate({
+        //     height:'100px'
+        // }, 600);
+      }  
+    }
+  });
 });
